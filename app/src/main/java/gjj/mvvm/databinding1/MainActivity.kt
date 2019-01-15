f188b2b55006425ad433ea8c8a85813dc068b18c
顾晶晶 <jingjinggu@pptv.com>
2019-01-15 10:55:20 +0800

    [Feature]解决kotlin和databinding的冲突

--- app/src/main/java/gjj/mvvm/databinding1/MainActivity.kt
+++ app/src/main/java/gjj/mvvm/databinding1/MainActivity.kt
@@ -1,12 +1,20 @@
 package gjj.mvvm.databinding1
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import gjj.mvvm.databinding1.databinding.MainDataBind
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        val mainBinding = DataBindingUtil.setContentView<MainDataBind>(this, R.layout.activity_main)
+        //设置绑定数据
+//        mainBinding.mainData=MainData()
+        mainBinding.setVariable(BR.mainData,MainData())
+
+
     }
 }
---
+++ app/src/main/java/gjj/mvvm/databinding1/MainData.kt
@@ -0,0 +1,12 @@
+package gjj.mvvm.databinding1
+
+import java.io.Serializable
+
+/**
+ * 作者：jingjinggu on 2019/1/15 10:40
+ * 邮箱：jingjinggu@pptv.com
+ */
+class MainData :Serializable{
+
+    var name:String?="null"
+}
