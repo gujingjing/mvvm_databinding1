ff0c299e550e0b827373902e8bbc8f4f95809167
顾晶晶 <jingjinggu@pptv.com>
2019-01-16 15:28:18 +0800

    [Feature]数据事件绑定

--- app/src/main/java/gjj/mvvm/databinding1/MainActivity.kt
+++ app/src/main/java/gjj/mvvm/databinding1/MainActivity.kt
@@ -1,20 +1,47 @@
 package gjj.mvvm.databinding1
 
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableArrayMap
+import android.databinding.ObservableMap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import gjj.mvvm.databinding1.databinding.MainDataBind
 
+
 class MainActivity : AppCompatActivity() {
+
+    var mainBinding:MainDataBind?=null
+    val mainData by lazy {  ObservableArrayMap<String,Any?>() }
+    val mainPresent by lazy {  MainPresent() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        val mainBinding = DataBindingUtil.setContentView<MainDataBind>(this, R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView<MainDataBind>(this, R.layout.activity_main)
         //设置绑定数据
 //        mainBinding.mainData=MainData()
-        mainBinding.setVariable(BR.mainData,MainData())
+        mainBinding?.setVariable(BR.mainData,mainData)
+        //设置事件数据
+        mainBinding?.setVariable(BR.mainPresent,mainPresent)
+    }
 
 
+    inner class MainPresent {
+
+        fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            mainData["clickText"] = s.toString()
+        }
+
+        fun onClick(view: View) {
+            Toast.makeText(this@MainActivity, "点到了", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onClickListenerBinding(mainData: ObservableMap<String,Any>) {
+            Toast.makeText(this@MainActivity, mainData["clickText"]?.toString()?:"",
+                    Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
--- app/src/main/java/gjj/mvvm/databinding1/MainData.kt
+++ app/src/main/java/gjj/mvvm/databinding1/MainData.kt
@@ -1,12 +1,15 @@
 package gjj.mvvm.databinding1
 
-import java.io.Serializable
+import android.databinding.ObservableField
 
 /**
  * 作者：jingjinggu on 2019/1/15 10:40
  * 邮箱：jingjinggu@pptv.com
  */
-class MainData :Serializable{
+class MainData {
 
-    var name:String?="null"
+    var name:ObservableField<String?>? = ObservableField("null-默认名称")
+
+    var clickText:ObservableField<String?>? = ObservableField("clickText-点击测试")
+
 }
